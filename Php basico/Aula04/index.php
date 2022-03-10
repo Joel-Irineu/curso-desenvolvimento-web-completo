1537446d535db5dc812fedc34c9aff9261ebe1c9
irineu <irineu.joel01@gmail.com>
2022-03-10 15:29:40 -0300

    Mais sobre variaveis

---
+++ Php basico/Aula04/index.php
@@ -0,0 +1,17 @@
+<?php
+    // String
+    $nome = 'Irineu'; 
+    // Numero
+    $idade = 20;
+    // Boleana
+    $bool = true;
+    // double
+    $double = 10.09;
+
+
+    echo 'Meu nome é '.$nome.'<br>';
+    echo 'Minha idade é '.$idade.'<br>';
+    echo $bool.'<br>';
+    echo $double.'<br>';
+
+?>
